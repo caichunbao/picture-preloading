8343479f68889f16f8b86c807ffb9126f3cb1b59
caichunbao <vipcaichunbao@163.com>
2018-04-24 23:46:43 +0800

    final commit

--- build/js/core.js
+++ build/js/core.js
@@ -10,20 +10,20 @@
 
 
 /**
+ * DEFAULT
+ *  order : 判断是有序预加载还是无序预加载
+ *  each  : 加载完一张之后的回调函数
+ *  all   : 全部都加载完之后的回调函数
+ * 
  * 1.有序预加载
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *  $.preload(imgs,{
+ *    order:"ordered"
+ *  })
  * 
  * 2.无序预加载
- * 
- * 
- * 
+ *  $.preload(imgs,{
+ *    order:"unordered"
+ *  })
  * 
  */
 
@@ -32,7 +32,40 @@
   function PreLoad(imgs, options) {
     this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
     this.opts = $.extend({}, PreLoad.DEFAULT, options);
-    this._unordered()
+    if (this.opts.order === "ordered") {
+      this._ordered()
+    } else {
+      this._unordered()
+    }
+  }
+
+  PreLoad.DEFAULT = {
+    each: null, // 每一个加载完之后
+    all: null, // 所有的都加载好之后
+    order: "unordered" //无序预加载
+  }
+
+  PreLoad.prototype._ordered = function() {
+    var imgs = this.imgs,
+      opts = this.opts,
+      count = 0,
+      len = imgs.length;
+    load()
+      // 有序预加载
+    function load() {
+      var imgObj = new Image();
+      $(imgObj).on("load error", function() {
+        opts.each && opts.each(count)
+        if (count >= len) {
+          // 所有图片已经加载完
+          opts.all && opts.all()
+        } else {
+          load()
+        }
+        count++
+      })
+      imgObj.src = imgs[count]
+    }
   }
 
   PreLoad.prototype._unordered = function() {
@@ -40,31 +73,18 @@
       opts = this.opts,
       count = 0,
       len = imgs.length;
-
     $.each(imgs, function(i, src) {
-
       if (typeof src != 'string') return
-
       var imgObj = new Image();
-
       $(imgObj).on("load error", function() {
-
         opts.each && opts.each(count);
-
         if (count >= len - 1) {
-
           opts.all && opts.all()
         }
-
         count++;
       })
       imgObj.src = src
     })
-  }
-
-  PreLoad.DEFAULT = {
-    each: null,
-    all: null
   }
 
   $.extend({
@@ -72,7 +92,5 @@
       return new PreLoad(imgs, opts)
     }
   })
-
-
 
 })(jQuery)
